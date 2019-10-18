88a7d933004a3b086eb029f62d3f9d8bff4dba53
Pavel Ozhich <pavel2.ozhich@gmail.com>
2019-10-18 11:17:31 +0300

    error with img

--- src/bsu/rfe/java/group6/lab2/Ozhich/varC2/MainFraim.java
+++ src/bsu/rfe/java/group6/lab2/Ozhich/varC2/MainFraim.java
@@ -1,10 +1,14 @@
 package bsu.rfe.java.group6.lab2.Ozhich.varC2;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import static java.lang.Math.*;
 
 public class MainFraim extends JFrame {
 
@@ -13,8 +17,17 @@ public class MainFraim extends JFrame {
 
     private JTextField textFieldX;
     private JTextField textFieldY;
+    private JTextField textFieldZ;
 
     private JTextField textFieldResult;
+
+    JPanel formulaImg = new JPanel() {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(img1, WIDTH / 2, 0, null);
+        }
+    };
 
     private ButtonGroup radioButtons = new ButtonGroup();
 
@@ -22,12 +35,15 @@ public class MainFraim extends JFrame {
 
     private int formulaId = 1;
 
-    public Double calculate1 (Double x, Double y) {
-        return x * x + y * y;
+    private Image img1 = ImageIO.read(new File("src/img1.bmp"));
+    private Image img2 = ImageIO.read(new File("src/img2.bmp"));
+
+    public Double calculate1 (Double x, Double y, Double z) {
+        return pow(pow(log(1 + x), 2) + cos(PI * pow(z, 3)), sin(y)) + pow(pow(E, pow(x, 2)) + cos(pow(E, z)) + sqrt(1 / y), 1 / x);
     }
 
-    public Double calculate2 (Double x, Double y) {
-        return x * x * x + 1 / y;
+    public Double calculate2 (Double x, Double y, Double z) {
+        return x * x * x + 1 / y;           // ЗАМЕНИТЬ ФОРМУЛУ
     }
 
     private void addRadioButton (String buttonName, final int formulaId) {
@@ -35,41 +51,72 @@ public class MainFraim extends JFrame {
         button.addActionListener(new ActionListener() {
             public void actionPerformed (ActionEvent ev) {
                 MainFraim.this.formulaId = formulaId;
-                // imagePane.updateUI();
+                // вывод изображения в зависимости от выбранной кнопки
+                if (MainFraim.this.formulaId == 1) {
+                    formulaImg = new JPanel() {
+                        @Override
+                        protected void paintComponent(Graphics g) {
+                            super.paintComponent(g);
+                            g.drawImage(img1, WIDTH / 2, 0, null);
+                        }
+                    };
+                    repaint();
+                }
+                if (MainFraim.this.formulaId == 2) {
+                    formulaImg = new JPanel() {
+                        @Override
+                        protected void paintComponent(Graphics g) {
+                            super.paintComponent(g);
+                            g.drawImage(img2, WIDTH / 2, 0, null);
+                        }
+                    };
+                    repaint();
+                }
             }
         });
         radioButtons.add(button);
         hboxFormulaType.add(button);
+
     }
 
-    public MainFraim () {
+    public MainFraim () throws IOException {
         super("Вычисление формулы");
         setSize(WIDTH, HEIGHT);
         Toolkit kit = Toolkit.getDefaultToolkit();
         setLocation((kit.getScreenSize().width - WIDTH) / 2, (kit.getScreenSize().height - HEIGTH) / 2);
+        // box с формулами
         hboxFormulaType.add(Box.createHorizontalGlue());
         addRadioButton("Формула 1", 1);
         addRadioButton("Формула 2", 2);
         radioButtons.setSelected(radioButtons.getElements().nextElement().getModel(), true);
         hboxFormulaType.add(Box.createHorizontalGlue());
         hboxFormulaType.setBorder(BorderFactory.createLineBorder(Color.YELLOW));
+        // box для значенией x, y, z
         JLabel labelForX = new JLabel("X:");
         textFieldX = new JTextField("0", 10);
         textFieldX.setMaximumSize(textFieldX.getPreferredSize());
         JLabel labelForY = new JLabel("Y:");
         textFieldY = new JTextField("0", 10);
         textFieldY.setMaximumSize(textFieldY.getPreferredSize());
+        JLabel labelForZ = new JLabel("Z:");
+        textFieldZ = new JTextField("0", 10);
+        textFieldZ.setMaximumSize(textFieldZ.getPreferredSize());
         Box hboxVariables = Box.createHorizontalBox();
         hboxVariables.setBorder(BorderFactory.createLineBorder(Color.RED));
         hboxVariables.add(Box.createHorizontalGlue());
         hboxVariables.add(labelForX);
         hboxVariables.add(Box.createHorizontalStrut(10));
         hboxVariables.add(textFieldX);
-        hboxVariables.add(Box.createHorizontalStrut(100));
+        hboxVariables.add(Box.createHorizontalStrut(30));
         hboxVariables.add(labelForY);
         hboxVariables.add(Box.createHorizontalStrut(10));
         hboxVariables.add(textFieldY);
+        hboxVariables.add(Box.createHorizontalStrut(30));
+        hboxVariables.add(labelForZ);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldZ);
         hboxVariables.add(Box.createHorizontalGlue());
+        // box для результата
         JLabel labelForResult = new JLabel("Результат:");
         textFieldResult = new JTextField("0", 10);
         textFieldResult.setMaximumSize(textFieldResult.getPreferredSize());
@@ -86,11 +133,12 @@ public class MainFraim extends JFrame {
                 try {
                     Double x = Double.parseDouble(textFieldX.getText());
                     Double y = Double.parseDouble(textFieldY.getText());
+                    Double z = Double.parseDouble(textFieldZ.getText());
                     Double result;
                     if (formulaId==1)
-                        result = calculate1(x, y);
+                        result = calculate1(x, y, z);
                     else
-                        result = calculate2(x, y);
+                        result = calculate2(x, y, z);
                     textFieldResult.setText(result.toString());
             } catch (NumberFormatException ex) {
                     JOptionPane.showMessageDialog(MainFraim.this, "Ошибка в формате записи числа с плавающей точкой", "Ошибочный формат числа",
@@ -106,6 +154,7 @@ public class MainFraim extends JFrame {
                 textFieldResult.setText("0");
             }
         });
+        // box для кнопок очистки и вычисления результата
         Box hboxButtons = Box.createHorizontalBox();
         hboxButtons.add(Box.createHorizontalGlue());
         hboxButtons.add(buttonCalc);
@@ -113,7 +162,14 @@ public class MainFraim extends JFrame {
         hboxButtons.add(buttonReset);
         hboxButtons.add(Box.createHorizontalGlue());
         hboxButtons.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+        // сборка всех box в один вертикальный box
         Box contentBox = Box.createVerticalBox();
+        contentBox.add(Box.createVerticalGlue());
+        Box hboxImg = Box.createHorizontalBox();
+        hboxImg.add(Box.createVerticalGlue());
+        hboxImg.add(formulaImg);
+        hboxImg.add(Box.createVerticalGlue());
+        contentBox.add(hboxImg);
         contentBox.add(Box.createVerticalGlue());
         contentBox.add(hboxFormulaType);
         contentBox.add(hboxVariables);
@@ -123,7 +179,7 @@ public class MainFraim extends JFrame {
         getContentPane().add(contentBox, BorderLayout.CENTER);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         MainFraim frame = new MainFraim();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
